7a35d116146b3032a2a47c022605702c8aea435f
Thomas Eichberger <thomas.eichberger@gmail.com>
2012-01-15 22:28:56 +0100

    Table header in seek All Data.

--- joomla/com_seek/site/views/seek/tmpl/default.php
+++ joomla/com_seek/site/views/seek/tmpl/default.php
@@ -4,7 +4,23 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 require_once 'taportal/harvester/_Classes.php';
 
 //
+//
+//
 echo "<table border='0' cellpadding='1' cellspacing='1'>\n";
+
+if ( $this->institutes->size() >= 2 )
+{
+  echo "<tr>";
+  echo "<td colspan='5'>" . $this->institutes->size() . " institutes</td>";
+  echo "</tr>";
+}
+else if ( $this->institutes->size() == 1 )
+{
+  echo "<tr>";
+  echo "<td colspan='5'>1 institute</td>";
+  echo "</tr>";
+}
+
 for( $i = 0; $i < $this->institutes->size(); $i++ )
 {
   $x = $this->institutes->get( $i );
@@ -19,10 +35,26 @@ for( $i = 0; $i < $this->institutes->size(); $i++ )
     echo "</tr>\n";
   }
 }
-echo "</table>\n";
+echo "</table><br><br>\n";
 
 //
+//
+//
 echo "<table border='0' cellpadding='1' cellspacing='1'>\n";
+
+if ( $this->experts->size() >= 2 )
+{
+  echo "<tr>";
+  echo "<td colspan='5'>" . $this->experts->size() . " experts</td>";
+  echo "</tr>";
+}
+else if ( $this->experts->size() == 1 )
+{
+  echo "<tr>";
+  echo "<td colspan='5'>1 expert</td>";
+  echo "</tr>";
+}
+
 for( $i = 0; $i < $this->experts->size(); $i++ )
 {
   $x = $this->experts->get( $i );
@@ -40,10 +72,26 @@ for( $i = 0; $i < $this->experts->size(); $i++ )
     echo "</tr>\n";
   }
 }
-echo "</table>\n";
+echo "</table><br><br>\n";
 
 //
+//
+//
 echo "<table border='0' cellpadding='1' cellspacing='1'>\n";
+
+if ( $this->projects->size() >= 2 )
+{
+  echo "<tr>";
+  echo "<td colspan='5'>" . $this->projects->size() . " projects</td>";
+  echo "</tr>";
+}
+else if ( $this->projects->size() == 1 )
+{
+  echo "<tr>";
+  echo "<td colspan='5'>1 project</td>";
+  echo "</tr>";
+}
+
 for( $i = 0; $i < $this->projects->size(); $i++ )
 {
   $x = $this->projects->get( $i );
@@ -60,10 +108,26 @@ for( $i = 0; $i < $this->projects->size(); $i++ )
     echo "</tr>\n";
   }
 }
-echo "</table>\n";
+echo "</table><br><br>\n";
 
 //
+//
+//
 echo "<table border='0' cellpadding='1' cellspacing='1'>\n";
+
+if ( $this->publications->size() >= 2 )
+{
+  echo "<tr>";
+  echo "<td colspan='5'>" . $this->publications->size() . " publications</td>";
+  echo "</tr>";
+}
+else if ( $this->publications->size() == 1 )
+{
+  echo "<tr>";
+  echo "<td colspan='5'>1 publication</td>";
+  echo "</tr>";
+}
+
 for( $i = 0; $i < $this->publications->size(); $i++ )
 {
   $x = $this->publications->get( $i );
@@ -77,6 +141,6 @@ for( $i = 0; $i < $this->publications->size(); $i++ )
     echo "</tr>\n";
   }
 }
-echo "</table>\n";
+echo "</table><br><br>\n";
 
 ?>

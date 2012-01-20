0a68e59a828bcad6cff9d934555a09ccfeeb61b5
Thomas Eichberger <thomas.eichberger@gmail.com>
2012-01-20 16:22:17 +0100

    Step

--- SysAdmin/Dev-Scripts/copy-comseek-to-local-webserver.cmd
+++ SysAdmin/Dev-Scripts/copy-comseek-to-local-webserver.cmd
@@ -1,2 +1,1 @@
-xcopy C:\Workspace-Zend\TA_Portal\joomla\com_seek\site\*.* C:\SWD\xampp\htdocs\joomla\components\com_seek\ /S/Q/R/Y
-pause
+@xcopy C:\Workspace-Zend\TA_Portal\joomla\com_seek\site\*.* C:\SWD\xampp\htdocs\joomla\components\com_seek\ /S/Q/R/Y
--- joomla/com_seek/site/views/seek/tmpl/default.php
+++ joomla/com_seek/site/views/seek/tmpl/default.php
@@ -68,297 +68,27 @@
 -->
 </style><?php
 
-$prefix = $_SERVER[ 'SERVER_NAME' ] == 'localhost' ? '/joomla' : '';
-
-?>
-<script
-	src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<script
-	src="<?php
-echo $prefix;
-?>/media/media/js/jquery.colorbox-min.js"></script>
-<link rel="stylesheet"
-	href="<?php
-echo $prefix;
-?>/media/media/css/colorbox.css" />
 
 
-
-
-<?php
-// No direct access to this file
-defined( '_JEXEC' ) or die( 'Restricted access' );
-require_once 'taportal/harvester/_Classes.php';
-
-// ----------------------------------------------------------------------
-// Counter for DIVs with additional information which are shown by colorbox/lightbox/jQuery
-//$lightbox = 0;
-
-
-// ----------------------------------------------------------------------
-$kind = $_GET[ 'kind' ];
-if ( is_null( $kind ) or $kind == '' or $kind == 'a' )
+function t( $value )
 {
-  $showI = TRUE;
-  $showE = TRUE;
-  $showR = TRUE;
-  $showU = TRUE;
-}
-else if ( $kind == 'i' )
-{
-  $showI = TRUE;
-  $showE = FALSE;
-  $showR = FALSE;
-  $showU = FALSE;
-}
-else if ( $kind == 'e' )
-{
-  $showI = FALSE;
-  $showE = TRUE;
-  $showR = FALSE;
-  $showU = FALSE;
-}
-else if ( $kind == 'r' )
-{
-  $showI = FALSE;
-  $showE = FALSE;
-  $showR = TRUE;
-  $showU = FALSE;
-}
-else if ( $kind == 'u' )
-{
-  $showI = FALSE;
-  $showE = FALSE;
-  $showR = FALSE;
-  $showU = TRUE;
+  if ( is_null( $value ) )
+  {
+    return '&nbsp;';
+  }
+  else
+  {
+    return $value;
+  }
 }
 
-// ----------------------------------------------------------------------
-//
-//
-//
-if ( $showI )
-{
-  echo "<table class='seektable' border='0' cellpadding='1' cellspacing='1' width='100%'>\n";
-  
-  if ( $this->institutes->size() >= 2 )
-  {
-    echo "<tr class='seektr'>";
-    echo "<td class='seektdhead' colspan='5'>" . $this->institutes->size() . " institutes</td>";
-    echo "</tr>";
-  }
-  else if ( $this->institutes->size() == 1 )
-  {
-    echo "<tr class='seektr'>";
-    echo "<td class='seektdhead' colspan='5'>1 institute</td>";
-    echo "</tr>";
-  }
-  
-  if ( $this->institutes->size() >= 1 )
-  {
-    echo "<tr class='seektrlabels'>";
-    echo "<td class='seektdlabels'>Abbr.</td>";
-    echo "<td class='seektdlabels'>Name</td>";
-    echo "<td class='seektdlabels'>Country</td>";
-    echo "<td class='seektdlabels'>Description</td>";
-    echo "<td class='seektdlabels'>Web</td>";
-    echo "</tr>\n";
-  }
-  
-  for( $i = 0; $i < $this->institutes->size(); $i++ )
-  {
-    $x = $this->institutes->get( $i );
-    if ( !is_null( $x ) )
-    {
-      if ( $i % 2 == 1 )
-        echo "<tr class='seektr2'>";
-      else
-        echo "<tr class='seektr'>";
-      echo "<td class='seektd'>" . t( $x->getAbbreviation() ) . "</td>";
-      echo "<td class='seektd'><b>" . t( $x->getName() ) . "</b></td>";
-      echo "<td class='seektd'>" . t( $x->getCountryCode() ) . "</td>";
-      echo "<td class='seektd'>" . t( $x->getDescription() ) . "</td>";
-      echo "<td class='seektd'>" . url( $x->getURL() ) . "</td>";
-      echo "</tr>\n";
-    }
-  }
-  echo "</table><br><br>\n";
-}
-
-//
-//
-//
-if ( $showE )
-{
-  echo "<table class='seektable' border='0' cellpadding='1' cellspacing='1' width='100%'>\n";
-  
-  if ( $this->experts->size() >= 2 )
-  {
-    echo "<tr class='seektr'>";
-    echo "<td class='seektdhead' colspan='7'>" . $this->experts->size() . " experts</td>";
-    echo "</tr>";
-  }
-  else if ( $this->experts->size() == 1 )
-  {
-    echo "<tr class='seektr'>";
-    echo "<td class='seektdhead' colspan='7'>1 expert</td>";
-    echo "</tr>";
-  }
-  
-  if ( $this->experts->size() >= 1 )
-  {
-    echo "<tr class='seektrlabels'>";
-    echo "<td class='seektdlabels'>Surname</td>";
-    echo "<td class='seektdlabels'>First name(s)</td>";
-    echo "<td class='seektdlabels'>Title</td>";
-    echo "<td class='seektdlabels'>E-Mail</td>";
-    echo "<td class='seektdlabels'>Phone number</td>";
-    echo "<td class='seektdlabels'>Skype ID</td>";
-    echo "<td class='seektdlabels'>Expertise</td>";
-    //echo "<td class='seektdlabels'>Institute</td>";
-    echo "</tr>\n";
-  }
-  
-  for( $i = 0; $i < $this->experts->size(); $i++ )
-  {
-    $x = $this->experts->get( $i );
-    if ( !is_null( $x ) )
-    {
-      if ( $i % 2 == 1 )
-        echo "<tr class='seektr2'>";
-      else
-        echo "<tr class='seektr'>";
-      echo "<td class='seektd'><b>" . t( $x->getSurname() ) . "</b></td>";
-      echo "<td class='seektd'><b>" . t( $x->getFirstnames() ) . "</b></td>";
-      echo "<td class='seektd'>" . t( $x->getExpTitle() ) . "</td>";
-      echo "<td class='seektd'>" . email( $x->getEMail() ) . "</td>";
-      echo "<td class='seektd'>" . t( $x->getPhoneNumber() ) . "</td>";
-      echo "<td class='seektd'>" . t( $x->getSkypeID() ) . "</td>";
-      echo "<td class='seektd'>" . t( $x->getExpertise() ) . "</td>";
-      //echo "<td class='seektd'>" . institute( $x->getInstitute() ) . "</td>";
-      echo "</tr>\n";
-      echo rowWithTableForInstitute( $x->getInstitute(), 7 );
-    }
-  }
-  echo "</table><br><br>\n";
-}
-
-//
-//
-//
-if ( $showR )
-{
-  echo "<table class='seektable' border='0' cellpadding='1' cellspacing='1' width='100%'>\n";
-  
-  if ( $this->projects->size() >= 2 )
-  {
-    echo "<tr class='seektr'>";
-    echo "<td class='seektdhead' colspan='7'>" . $this->projects->size() . " projects</td>";
-    echo "</tr>";
-  }
-  else if ( $this->projects->size() == 1 )
-  {
-    echo "<tr class='seektr'>";
-    echo "<td class='seektdhead' colspan='7'>1 project</td>";
-    echo "</tr>";
-  }
-  
-  if ( $this->projects->size() >= 1 )
-  {
-    echo "<tr class='seektrlabels'>";
-    echo "<td class='seektdlabels'>Short Title</td>";
-    echo "<td class='seektdlabels'>Long Title</td>";
-    echo "<td class='seektdlabels'>Description</td>";
-    echo "<td class='seektdlabels'>Start</td>";
-    echo "<td class='seektdlabels'>End</td>";
-    echo "<td class='seektdlabels'>Home Page</td>";
-    echo "<td class='seektdlabels'>Contact Person</td>";
-    echo "</tr>\n";
-  }
-  
-  for( $i = 0; $i < $this->projects->size(); $i++ )
-  {
-    $x = $this->projects->get( $i );
-    if ( !is_null( $x ) )
-    {
-      if ( $i % 2 == 1 )
-        echo "<tr class='seektr2'>";
-      else
-        echo "<tr class='seektr'>";
-      echo "<td class='seektd'><b>" . t( $x->getShortTitleE() ) . "</b></td>";
-      echo "<td class='seektd'>" . t( $x->getLongTitleE() ) . "</td>";
-      echo "<td class='seektd'>" . t( $x->getShortDescriptionE() ) . "</td>";
-      echo "<td class='seektd'>" . t( $x->getStartDate() ) . "</td>";
-      echo "<td class='seektd'>" . t( $x->getEndDate() ) . "</td>";
-      echo "<td class='seektd'>" . url( $x->getHomePage() ) . "</td>";
-      echo "<td class='seektd'>" . expert( $x->getContactPerson() ) . "</td>";
-      echo "</tr>\n";
-    }
-  }
-  echo "</table><br><br>\n";
-}
-
-//
-//
-//
-if ( $showU )
-{
-  echo "<table class='seektable' border='0' cellpadding='1' cellspacing='1' width='100%'>\n";
-  
-  if ( $this->publications->size() >= 2 )
-  {
-    echo "<tr class='seektr'>";
-    echo "<td class='seektdhead' colspan='3'>" . $this->publications->size() . " publications</td>";
-    echo "</tr>";
-  }
-  else if ( $this->publications->size() == 1 )
-  {
-    echo "<tr class='seektr'>";
-    echo "<td class='seektdhead' colspan='3'>1 publication</td>";
-    echo "</tr>";
-  }
-  
-  if ( $this->publications->size() >= 1 )
-  {
-    echo "<tr class='seektrlabels'>";
-    echo "<td class='seektdlabels'>Quotation</td>";
-    echo "<td class='seektdlabels'>Publ. Date</td>";
-    echo "<td class='seektdlabels'>Type</td>";
-    //echo "<td class='seektdlabels'>Institute</td>";
-    echo "</tr>\n";
-  }
-  
-  for( $i = 0; $i < $this->publications->size(); $i++ )
-  {
-    $x = $this->publications->get( $i );
-    if ( !is_null( $x ) )
-    {
-      if ( $i % 2 == 1 )
-        echo "<tr class='seektr2'>";
-      else
-        echo "<tr class='seektr'>";
-      echo "<td class='seektd'><b>" . t( $x->getQuotation() ) . "</b></td>";
-      echo "<td class='seektd'>" . t( $x->getPublDate() ) . "</td>";
-      echo "<td class='seektd'>" . t( $x->getPublType()->value() ) . "</td>";
-      //      echo "<td class='seektd'>" . institute( $x->getInstitute() ) . "</td>";
-      echo "</tr>\n";
-      echo rowWithTableForInstitute( $x->getInstitute(), 3 );
-    }
-  }
-  echo "</table><br><br>\n";
-}
-
-
-
-//function institute( Institute $institute/*, $lightbox */)
-//{
-//  return lightboxForInstitute( $institute, $lightbox );
-//}
 
 
 function rowWithTableForInstitute( Institute $institute, $cols )
 {
-  $html = "<tr><td colspan='{$cols}'>" . /***/
+  $columns = $cols - 1;
+  
+  $html = "<tr><td>&nbsp;</td><td colspan='{$columns}'>" . /***/
   makeDivTableForInstitute( $institute ) . /***/
   "</td></tr>";
   
@@ -369,8 +99,8 @@ function rowWithTableForInstitute( Institute $institute, $cols )
 
 function makeDivTableForInstitute( Institute $institute )
 {
-  $html = "<div style='font-size:90%, padding:10px; background:#E0E0E0; border-width:2px; border-style:solid; border-color:#000000;'>" . /***/
-  '<table border="1" width="100%" cellpadding="2" cellspacing="2">' . /***/
+  $html = "<div style='font-size:90%, padding:10px; border-width:1px; border-style:solid; border-color:#CCCCCC;'>" . /***/
+  '<table border="1" cellpadding="2" cellspacing="2">' . /***/
   '<tr>' . /***/
   '<td valign="top" colspan="2"><div style="text-align:center;text-weight:bold;">' . 'Institute' . '</div></td>' . /***/
   '</tr>' . /***/
@@ -486,19 +216,299 @@ function shortenURL( $url )
   return $url2;
 }
 
+// ===================================================================
+// ===================================================================
+// ===================================================================
+// ===================================================================
 
 
-function t( $value )
+try
 {
-  if ( is_null( $value ) )
+  $prefix = $_SERVER[ 'SERVER_NAME' ] == 'localhost' ? '/joomla' : '';
+  
+  ?>
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script
+	src="<?php
+  echo $prefix;
+  ?>/media/media/js/jquery.colorbox-min.js"></script>
+<link rel="stylesheet"
+	href="<?php
+  echo $prefix;
+  ?>/media/media/css/colorbox.css" />
+
+
+
+
+<?php
+  // No direct access to this file
+  defined( '_JEXEC' ) or die( 'Restricted access' );
+  require_once 'taportal/harvester/_Classes.php';
+  
+  // ----------------------------------------------------------------------
+  // Counter for DIVs with additional information which are shown by colorbox/lightbox/jQuery
+  //$lightbox = 0;
+  
+
+  // ----------------------------------------------------------------------
+  $kind = $_GET[ 'kind' ];
+  if ( is_null( $kind ) or $kind == '' or $kind == 'a' )
   {
-    return '&nbsp;';
+    $showI = TRUE;
+    $showE = TRUE;
+    $showR = TRUE;
+    $showU = TRUE;
   }
-  else
+  else if ( $kind == 'i' )
   {
-    return $value;
+    $showI = TRUE;
+    $showE = FALSE;
+    $showR = FALSE;
+    $showU = FALSE;
   }
-}
+  else if ( $kind == 'e' )
+  {
+    $showI = FALSE;
+    $showE = TRUE;
+    $showR = FALSE;
+    $showU = FALSE;
+  }
+  else if ( $kind == 'r' )
+  {
+    $showI = FALSE;
+    $showE = FALSE;
+    $showR = TRUE;
+    $showU = FALSE;
+  }
+  else if ( $kind == 'u' )
+  {
+    $showI = FALSE;
+    $showE = FALSE;
+    $showR = FALSE;
+    $showU = TRUE;
+  }
+  
+  // ----------------------------------------------------------------------
+  //
+  //
+  //
+  if ( $showI )
+  {
+    echo "<table class='seektable' border='0' cellpadding='1' cellspacing='1' width='100%'>\n";
+    
+    if ( $this->institutes->size() >= 2 )
+    {
+      echo "<tr class='seektr'>";
+      echo "<td class='seektdhead' colspan='5'>" . $this->institutes->size() . " institutes</td>";
+      echo "</tr>";
+    }
+    else if ( $this->institutes->size() == 1 )
+    {
+      echo "<tr class='seektr'>";
+      echo "<td class='seektdhead' colspan='5'>1 institute</td>";
+      echo "</tr>";
+    }
+    
+    if ( $this->institutes->size() >= 1 )
+    {
+      echo "<tr class='seektrlabels'>";
+      echo "<td class='seektdlabels'>Abbr.</td>";
+      echo "<td class='seektdlabels'>Name</td>";
+      echo "<td class='seektdlabels'>Country</td>";
+      echo "<td class='seektdlabels'>Description</td>";
+      echo "<td class='seektdlabels'>Web</td>";
+      echo "</tr>\n";
+    }
+    
+    for( $i = 0; $i < $this->institutes->size(); $i++ )
+    {
+      $x = $this->institutes->get( $i );
+      if ( !is_null( $x ) )
+      {
+        if ( $i % 2 == 1 )
+          echo "<tr class='seektr2'>";
+        else
+          echo "<tr class='seektr'>";
+        echo "<td class='seektd'>" . t( $x->getAbbreviation() ) . "</td>";
+        echo "<td class='seektd'><b>" . t( $x->getName() ) . "</b></td>";
+        echo "<td class='seektd'>" . t( $x->getCountryCode() ) . "</td>";
+        echo "<td class='seektd'>" . t( $x->getDescription() ) . "</td>";
+        echo "<td class='seektd'>" . url( $x->getURL() ) . "</td>";
+        echo "</tr>\n";
+      }
+    }
+    echo "</table><br><br>\n";
+  }
+  
+  //
+  //
+  //
+  if ( $showE )
+  {
+    echo "<table class='seektable' border='0' cellpadding='1' cellspacing='1' width='100%'>\n";
+    
+    if ( $this->experts->size() >= 2 )
+    {
+      echo "<tr class='seektr'>";
+      echo "<td class='seektdhead' colspan='7'>" . $this->experts->size() . " experts</td>";
+      echo "</tr>";
+    }
+    else if ( $this->experts->size() == 1 )
+    {
+      echo "<tr class='seektr'>";
+      echo "<td class='seektdhead' colspan='7'>1 expert</td>";
+      echo "</tr>";
+    }
+    
+    if ( $this->experts->size() >= 1 )
+    {
+      echo "<tr class='seektrlabels'>";
+      echo "<td class='seektdlabels'>Surname</td>";
+      echo "<td class='seektdlabels'>First name(s)</td>";
+      echo "<td class='seektdlabels'>Title</td>";
+      echo "<td class='seektdlabels'>E-Mail</td>";
+      echo "<td class='seektdlabels'>Phone number</td>";
+      echo "<td class='seektdlabels'>Skype ID</td>";
+      echo "<td class='seektdlabels'>Expertise</td>";
+      //echo "<td class='seektdlabels'>Institute</td>";
+      echo "</tr>\n";
+    }
+    
+    for( $i = 0; $i < $this->experts->size(); $i++ )
+    {
+      $x = $this->experts->get( $i );
+      if ( !is_null( $x ) )
+      {
+        if ( $i % 2 == 1 )
+          echo "<tr class='seektr2'>";
+        else
+          echo "<tr class='seektr'>";
+        echo "<td class='seektd'><b>" . t( $x->getSurname() ) . "</b></td>";
+        echo "<td class='seektd'><b>" . t( $x->getFirstnames() ) . "</b></td>";
+        echo "<td class='seektd'>" . t( $x->getExpTitle() ) . "</td>";
+        echo "<td class='seektd'>" . email( $x->getEMail() ) . "</td>";
+        echo "<td class='seektd'>" . t( $x->getPhoneNumber() ) . "</td>";
+        echo "<td class='seektd'>" . t( $x->getSkypeID() ) . "</td>";
+        echo "<td class='seektd'>" . t( $x->getExpertise() ) . "</td>";
+        //echo "<td class='seektd'>" . institute( $x->getInstitute() ) . "</td>";
+        echo "</tr>\n";
+        echo rowWithTableForInstitute( $x->getInstitute(), 7 );
+      }
+    }
+    echo "</table><br><br>\n";
+  }
+  
+  //
+  //
+  //
+  if ( $showR )
+  {
+    echo "<table class='seektable' border='0' cellpadding='1' cellspacing='1' width='100%'>\n";
+    
+    if ( $this->projects->size() >= 2 )
+    {
+      echo "<tr class='seektr'>";
+      echo "<td class='seektdhead' colspan='7'>" . $this->projects->size() . " projects</td>";
+      echo "</tr>";
+    }
+    else if ( $this->projects->size() == 1 )
+    {
+      echo "<tr class='seektr'>";
+      echo "<td class='seektdhead' colspan='7'>1 project</td>";
+      echo "</tr>";
+    }
+    
+    if ( $this->projects->size() >= 1 )
+    {
+      echo "<tr class='seektrlabels'>";
+      echo "<td class='seektdlabels'>Short Title</td>";
+      echo "<td class='seektdlabels'>Long Title</td>";
+      echo "<td class='seektdlabels'>Description</td>";
+      echo "<td class='seektdlabels'>Start</td>";
+      echo "<td class='seektdlabels'>End</td>";
+      echo "<td class='seektdlabels'>Home Page</td>";
+      echo "<td class='seektdlabels'>Contact Person</td>";
+      echo "</tr>\n";
+    }
+    
+    for( $i = 0; $i < $this->projects->size(); $i++ )
+    {
+      $x = $this->projects->get( $i );
+      if ( !is_null( $x ) )
+      {
+        if ( $i % 2 == 1 )
+          echo "<tr class='seektr2'>";
+        else
+          echo "<tr class='seektr'>";
+        echo "<td class='seektd'><b>" . t( $x->getShortTitleE() ) . "</b></td>";
+        echo "<td class='seektd'>" . t( $x->getLongTitleE() ) . "</td>";
+        echo "<td class='seektd'>" . t( $x->getShortDescriptionE() ) . "</td>";
+        echo "<td class='seektd'>" . t( $x->getStartDate() ) . "</td>";
+        echo "<td class='seektd'>" . t( $x->getEndDate() ) . "</td>";
+        echo "<td class='seektd'>" . url( $x->getHomePage() ) . "</td>";
+        echo "<td class='seektd'>" . expert( $x->getContactPerson() ) . "</td>";
+        echo "</tr>\n";
+      }
+    }
+    echo "</table><br><br>\n";
+  }
+  
+  //
+  //
+  //
+  if ( $showU )
+  {
+    echo "<table class='seektable' border='0' cellpadding='1' cellspacing='1' width='100%'>\n";
+    
+    if ( $this->publications->size() >= 2 )
+    {
+      echo "<tr class='seektr'>";
+      echo "<td class='seektdhead' colspan='3'>" . $this->publications->size() . " publications</td>";
+      echo "</tr>";
+    }
+    else if ( $this->publications->size() == 1 )
+    {
+      echo "<tr class='seektr'>";
+      echo "<td class='seektdhead' colspan='3'>1 publication</td>";
+      echo "</tr>";
+    }
+    
+    if ( $this->publications->size() >= 1 )
+    {
+      echo "<tr class='seektrlabels'>";
+      echo "<td class='seektdlabels'>Quotation</td>";
+      echo "<td class='seektdlabels'>Publ. Date</td>";
+      echo "<td class='seektdlabels'>Type</td>";
+      //echo "<td class='seektdlabels'>Institute</td>";
+      echo "</tr>\n";
+    }
+    
+    for( $i = 0; $i < $this->publications->size(); $i++ )
+    {
+      $x = $this->publications->get( $i );
+      if ( !is_null( $x ) )
+      {
+        if ( $i % 2 == 1 )
+          echo "<tr class='seektr2'>";
+        else
+          echo "<tr class='seektr'>";
+        echo "<td class='seektd'><b>" . t( $x->getQuotation() ) . "</b></td>";
+        echo "<td class='seektd'>" . t( $x->getPublDate() ) . "</td>";
+        echo "<td class='seektd'>" . t( $x->getPublType()->value() ) . "</td>";
+        //      echo "<td class='seektd'>" . institute( $x->getInstitute() ) . "</td>";
+        echo "</tr>\n";
+        echo rowWithTableForInstitute( $x->getInstitute(), 3 );
+      }
+    }
+    echo "</table><br><br>\n";
+  }
+
+  //function institute( Institute $institute/*, $lightbox */)
+//{
+//  return lightboxForInstitute( $institute, $lightbox );
+//}
+
 
 //function lightboxForInstitute( $institute, &$lightbox )
 //{
@@ -585,4 +595,10 @@ function t( $value )
 	} );
 </script>
 */
+
+}
+catch ( Exception $x )
+{
+  echo $x->getTraceAsString();
+}
 ?>
